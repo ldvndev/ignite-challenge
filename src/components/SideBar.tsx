56f02e3d9d22d9db8b1494878555067157cc3cb0
ldvndev <ldvndev@gmail.com>
2023-10-23 20:07:54 -0300

    refactor: sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,41 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+import { api } from "../services/api";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBaPros {
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ handleClickButton, selectedGenreId }: SideBaPros) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+  
   return(
-    <div>
-      
-    </div>
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
   );
 }
