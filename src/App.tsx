6d7c369be283d21223fec13b4df00501f3261f79
ldvndev <ldvndev@gmail.com>
2023-10-23 20:13:23 -0300

    feat: challenge-complete

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ interface GenreResponseProps {
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
-
+  
   useEffect(() => {
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -36,13 +36,22 @@ export function Content({ selectedGenreId, selectedGenre }: ContentProps) {
   return(
     <div className="container">
     <header>
-      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      <span className="category">
+        Categoria:
+        <span> {selectedGenre.title}</span>
+      </span>
     </header>
 
     <main>
       <div className="movies-list">
         {movies.map(movie => (
-          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          <MovieCard 
+            key ={movie.imdbID} 
+            title={movie.Title} 
+            poster={movie.Poster} 
+            runtime={movie.Runtime} 
+            rating={movie.Ratings[0].Value} 
+          />
         ))}
       </div>
     </main>
