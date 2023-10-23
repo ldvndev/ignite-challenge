d51edeb27d22bff1cb7a71dcd5469892bf9da846
ldvndev <ldvndev@gmail.com>
2023-10-23 19:59:41 -0300

    refactor: content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,7 @@
 export function Content() {
-  // Complete aqui
+  return(
+    <div>
+      
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,7 @@
 export function SideBar() {
-  // Complete aqui
+  return(
+    <div>
+      
+    </div>
+  );
 }
